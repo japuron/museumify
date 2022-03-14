047742af15e6d52abe3e0dfbda3e5faf777eb3a1
anonymous anonymous <jose.puron@kellogg.northwestern.edu>
2022-03-14 23:52:19 +0000

    Generate graphiti resources for artists

---
+++ app/resources/artist_resource.rb
@@ -0,0 +1,13 @@
+class ArtistResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :name, :string
+  attribute :dob_year, :integer
+  attribute :biography, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
