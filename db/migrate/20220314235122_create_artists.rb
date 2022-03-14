5eda295ef59a527f42fcdddbfec6ed2f0c172b36
anonymous anonymous <jose.puron@kellogg.northwestern.edu>
2022-03-14 23:51:23 +0000

    Generate artists resource with ideas:scaffold

---
+++ db/migrate/20220314235122_create_artists.rb
@@ -0,0 +1,11 @@
+class CreateArtists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.integer :dob_year
+      t.string :biography
+
+      t.timestamps
+    end
+  end
+end
